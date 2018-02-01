a86faf9a2110c399a18687ca5a271863ea7f9dad
Sam Guliker <samguliker@hotmail.com>
2018-02-01 09:56:00 +0100

    @ Add Senmetsu

---
+++ javascript/d3.js
@@ -0,0 +1,176 @@
+/* Selecteerd de ID en maakt een svg*/
+var svgBar = d3.select("#barchart"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svgBar.attr("width") - margin.left - margin.right,
+    height = +svgBar.attr("height") - margin.top - margin.bottom;
+
+/*  Scaleband word een nieuwe bandiwdth op de x as gemaakt  https://github.com/d3/d3-scale/blob/master/README.md#scaleBand */
+/* scaleLinear is een continious scale */
+var xBar = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    yBar = d3.scaleLinear().rangeRound([height, 0]);
+
+var gBar = svgBar.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var svgPie = d3.select("#piechart"),
+    widthPie = +svgPie.attr("width"),
+    heightPie = +svgPie.attr("height"),
+    radius = Math.min(widthPie, heightPie) / 2,
+    gPie = svgPie.append("g").attr("transform", "translate(" + widthPie / 2 + "," + heightPie / 2 + ")");
+
+var color = d3.scaleOrdinal(["#FFC636", "#FF6444"]);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.total_litres_of_pure_alcohol_servings; });
+
+var pathPie = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelPie = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+d3.csv("data/drinks.csv", function(drinks) {
+  drinks.total_litres_of_pure_alcohol_servings =+drinks.total_litres_of_pure_alcohol_servings;
+  drinks.beer_servings =+drinks.beer_servings;
+  drinks.spirit_servings =+drinks.spirit_servings;
+  drinks.wine_servings =+drinks.wine_servings;
+  return drinks;
+}, function(error, data) {
+  if (error) throw error;
+
+  /* De sort functies zorgen ervoor dat de hoogste getallen naar boven komen.
+   Met slice maak ik nieuwe array's, zodat de data niet aangepast wordt. */
+  beerData = data.sort(function(a, b){
+    return parseFloat(b.beer_servings) - parseFloat(a.beer_servings);
+  }).slice(0);
+
+  wineData = data.sort(function(a, b){
+    return parseFloat(b.wine_servings) - parseFloat(a.wine_servings);
+  }).slice(0);
+
+  totalAlc = data.sort(function(a, b){
+    return parseFloat(b.total_litres_of_pure_alcohol_servings) - parseFloat(a.total_litres_of_pure_alcohol_servings);
+  }).slice(0);
+
+  /* Pakt de top 5 uit de data. */
+  cleanedBeer = beerData.splice(0,5)
+  cleanedWine = wineData.splice(0,5)
+  cleanedTotal = totalAlc.splice(0,5)
+
+  /* Zorgt ervoor dat de juiste data word gebruikt voor de x en y as. */
+  xBar.domain(cleanedBeer.map(function(cleanedBeer) {
+        return cleanedBeer.country;
+    }));
+
+  /* de d3.max kijkt naar de hoogste waarde uit de data en gebruikt het. */
+  yBar.domain([0, d3.max(cleanedBeer, function(cleanedBeer) {
+      return cleanedBeer.beer_servings;
+  })]);
+
+
+  /* Geeft de groepeerde attributen in svgBar nog meer attributen */
+
+  /* x as voor de barchart */
+  gBar.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xBar));
+
+  /* add een label aan de rechter kant */
+  gBar.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Gallons");
+
+  /* y as voor de barchart */
+  gBar.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(yBar).ticks(10))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+
+/* add een label aan de bottom */
+  gBar.append("text")
+    .attr("transform",
+          "translate(" + (width/2) + " ," +
+                         (height + margin.top + 0) + ")")
+    .style("text-anchor", "middle")
+    .text("Country");
+
+  /*Voegt data en attributen toe aan alle bars in mijn svgBar*/
+  gBar.selectAll(".bar")
+    .data(cleanedBeer)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(cleanedBeer) {
+         return xBar(cleanedBeer.country);
+       })
+      .attr("y", function(cleanedBeer) {
+        return yBar(cleanedBeer.beer_servings);
+       })
+      .attr("width", xBar.bandwidth())
+      .attr("height", function(cleanedBeer) {
+        return height - yBar(cleanedBeer.beer_servings);
+      })
+
+    /* hover state */
+    .on("mouseover", function(d) {
+      handleMouseOver([{
+        value: d.beer_servings
+      },
+      {
+        value: d.wine_servings
+      }])
+    })
+
+    var arc = gPie.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+    arc.append("path")
+    arc.append("text")
+
+
+    function handleMouseOver(data){
+      var pie = d3.pie()
+          .sort(null)
+          .value(function(d) {return d.value;});
+
+      var arc = gPie.selectAll(".arc")
+        .data(pie(data))
+
+      arc.enter()
+        .append("g")
+        .attr("class", "arc")
+
+      arc.select("path")
+        .transition()
+        .duration(500)
+        .attr("d", function(d) {
+          return pathPie(d)
+        } )
+        .attr("fill", function(d) {
+          return color(d.value);
+        });
+
+      arc.select("text")
+          .attr("transform", function(d) { return "translate(" + labelPie.centroid(d) + ")"; })
+          .attr("dy", "0.35em")
+          .text(function(d) {
+            return d.value + ' G';
+          });
+
+      arc.exit().remove()
+
+    }
+});
--- readme.md
+++ readme.md
@@ -1,37 +1,96 @@
-# ![Assessment 3][banner]
+# Assessment 3: Alcohol Consumption
+![preview](preview.png)
 
-This repository can be forked for [**assessment 3**][a2] of [frontend 3][fe3]
-at [**@CMDA**][cmda].
+## Beschrijving
+Data veranderen dmv Join, Exit.
 
-## TODO
+## Background
+Ik heb voornamelijk mijn data opgeruimd met [replace](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace) hier word er gebruikt gemaakt van [regular expressions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
 
-*   [ ] [GitHub Pages](#github-pages)
-*   [ ] [Metadata](#metadata)
-*   [ ] [Issues](#issues)
-*   [ ] Replace this document in your fork with your own readme!
+## Stappen die ik heb genomen
+Gezorgd dat de data klaar staat en daarna veranderd kan worden.
 
-## GitHub Pages
+### Enter  & Exit
+[Enter](https://medium.com/@c_behrens/enter-update-exit-6cafc6014c36) zorgt ervoor dat de data geupdate kan worden.
+[Exit](https://medium.com/@c_behrens/enter-update-exit-6cafc6014c36) zorgt ervoor wanneer de data verdwijnt het ook echt uit de DOM word gehaald.
 
-Set up [GitHub Pages][pages] for this fork through the **Settings** pane.  Use
-the **Master branch** as its source.  Do not choose a Jekyll template.
+### Enter
+enter wacht nu op de data zodat het geupdate kan worden.
+```
+var arc = gPie.selectAll(".arc")
+.data(pie(data))
+.enter().append("g")
+  .attr("class", "arc");
 
-## Metadata
+};
 
-Edit the **description** and **url** of your repository.  Click on edit above
-the green Clone or download button and fill in a correct description and use the
-`github.io` URL you just set up.
+```
 
-## Issues
+### Mouse over
+On mouseover functie geeft de juiste data door aan  de  handleMouseOver functie
 
-Enable issues so we can give feedback by going to the settings tab of your fork
-and checking the box next to `issues`.
+```
+.on("mouseover", function(d) {
+  handleMouseOver([{
+    value: d.beer_servings
+  },
+  {
+    value: d.wine_servings
+  }])
+})
+```
 
-[banner]: https://cdn.rawgit.com/cmda-fe3/logo/a4b0614/banner-assessment-3.svg
+### Update
+Hier wordt de data geupdate.
+```
 
-[a2]: https://github.com/cmda-fe3/course-17-18/tree/master/assessment-3#description
+function handleMouseOver(data){
+  var pie = d3.pie()
+      .sort(null)
+      .value(function(d) {return d.value;});
 
-[fe3]: https://github.com/cmda-fe3
+  var arc = gPie.selectAll(".arc")
+    .data(pie(data))
 
-[cmda]: https://github.com/cmda
 
-[pages]: https://pages.github.com
+  arc.enter()
+    .append("g")
+    .attr("class", "arc")
+
+  arc.select("path")
+    .transition()
+    .duration(500)
+    .attr("d", function(d) {
+      return pathPie(d)
+    } )
+    .attr("fill", function(d) {
+      return color(d.value);
+    });
+
+  arc.select("text")
+      .attr("transform", function(d) { return "translate(" + labelPie.centroid(d) + ")"; })
+      .attr("dy", "0.35em")
+      .text(function(d) {
+        return d.value + 'G';
+      });
+
+      arc.exit().remove()
+
+}
+```
+
+
+## Dataset
+Dataset: https://github.com/fivethirtyeight/data/tree/master/alcohol-consumption
+
+## License
+MIT Sam Guliker
+
+## Features
+* [scaleBand](https://github.com/d3/d3-scale/blob/master/README.md#scaleBand)
+* [rangeRound](https://github.com/d3/d3-scale#band_rangeRound) -
+* [data binding](http://alignedleft.com/tutorials/d3/binding-data )
+* [domain](https://github.com/d3/d3-scale/blob/master/README.md#quantize_domain)
+* [range](https://github.com/d3/d3-scale/blob/master/README.md#quantize_range)
+* [ticks](https://github.com/d3/d3-scale/blob/master/README.md#quantize_ticks)
+* [scaleOrdinal](https://github.com/d3/d3-scale/blob/master/README.md#scaleOrdinal)
